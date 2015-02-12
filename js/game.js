e456facde6c1432fb1a2c1f4c5782b7849a497ab
poofytoo <victorhung92@gmail.com>
2015-02-12 17:26:51 -0500

    steve is still huge, but at least now only stomps on things when he walks. good steve.

--- js/game.js
+++ js/game.js
@@ -12,35 +12,35 @@ gameObj.Game.prototype = {
 
     this.base.resizeWorld();
     // this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
-
     // this.backgroundlayer.resizeWorld();
 
     this.state = {
-      debounce: 32,
-      velocity: 256,
+      velocity: 200,
       step: 32,
       moving: false,
       direction: null,
       next: null,
       direction: null
     }
-     //create player
+    
+    // Create player
     var result = this.findObjectsByType('steveStart', this.map, 'objectsLayer')
 
-    //we know there is just one result
+    // Initiate the player
     this.player = this.game.add.sprite(result[0].x, result[0].y, 'steve');
     this.game.physics.arcade.enable(this.player);
-
+    this.player.animations.add('run');
     this.player.anchor.setTo(0.5,0.5)
     this.player.scale.x = -1;
     this.player.position.setTo(this.player.position.x + 16,this.player.position.y + 16);
-
     this.game.camera.follow(this.player);
-    //move player with cursor keys
 
+    // Move player with cursor keys
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
+    // Add foreground
     this.front = this.map.createLayer('Front');
+    
   },
   //find objects in a Tiled layer that containt a property called "type" equal to a certain value
   findObjectsByType: function(type, map, layer) {
@@ -92,18 +92,40 @@ gameObj.Game.prototype = {
 
     this.player.body.velocity.y = 0;
     this.player.body.velocity.x = 0;
-    
+
+/*
+    if (this.state.moved) {
+      this.state.debounceTimer --;
+      if (this.state.debounceTimer > 0) {
+        return;
+      }
+
+      // TODO: There's probably a better way of checking for this
+      if (!this.cursors.up.isDown &&
+          !this.cursors.down.isDown &&
+          !this.cursors.left.isDown &&
+          !this.cursors.right.isDown) {
+        console.log('released keys')
+        this.state.moved = false;
+      }
+    }
+*/
+
+    // TODO: Should be switched to event listeners
+
     if (this.cursors.up.isDown) {
       this.state.moving = true;
       this.state.direction = 'up';
       this.state.next = this.player.body.y - this.state.step;
       this.player.body.velocity.y -= this.state.velocity;
+      this.player.animations.play('run', 15, true);
 
     } else if (this.cursors.down.isDown) {
       this.state.moving = true;
       this.state.direction = 'down';
       this.state.next = this.player.body.y + this.state.step;
       this.player.body.velocity.y += this.state.velocity;
+      this.player.animations.play('run', 15, true);
 
     } else if (this.cursors.left.isDown) {
       this.state.moving = true;
@@ -111,6 +133,7 @@ gameObj.Game.prototype = {
       this.state.next = this.player.body.x - this.state.step;
       this.player.body.velocity.x -= this.state.velocity;
       this.player.scale.x = 1;
+      this.player.animations.play('run', 15, true);
 
     } else if (this.cursors.right.isDown) {
       this.state.moving = true;
@@ -118,7 +141,11 @@ gameObj.Game.prototype = {
       this.state.next = this.player.body.x + this.state.step;
       this.player.body.velocity.x += this.state.velocity;
       this.player.scale.x = -1;
+      this.player.animations.play('run', 15, true);
 
+    } else {
+      // Setting TRUE should return to frame 1 when stopped
+      this.player.animations.stop(true);
     }
   }
 }
