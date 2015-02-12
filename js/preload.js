64cac64ab1ca110217c563d8b9fc2c4a7faf07a5
poofytoo <victorhung92@gmail.com>
2015-02-12 00:39:04 -0500

    added foreground

--- js/preload.js
+++ js/preload.js
@@ -4,7 +4,7 @@ gameObj.Preload = function(){};
 gameObj.Preload.prototype = {
   preload: function() {
     this.load.tilemap('level1', 'assets/tilemaps/tilemaptest.json', null, Phaser.Tilemap.TILED_JSON);
-    this.load.image('gameTiles', 'assets/raw/test-tile.png');
+    this.load.image('gameTiles', 'assets/raw/test-tile2.png');
   },
   create: function() {
     this.state.start('Game');
