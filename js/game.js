a18a5d692a07e8d5c531e110fcbd09407030748c
Stephanie Yu <styu314@gmail.com>
2015-02-12 00:10:13 -0500

    discrete steps

--- js/game.js
+++ js/game.js
@@ -12,5 +12,78 @@ gameObj.Game.prototype = {
     this.backgroundlayer = this.map.createLayer('Front');
 
     this.blockedLayer.resizeWorld();
+    // this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
+
+    this.backgroundlayer.resizeWorld();
+
+    this.state = {
+      moving: false,
+      direction: null,
+      step: 0
+    }
+     //create player
+    var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer')
+
+    //we know there is just one result
+    this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
+    this.game.physics.arcade.enable(this.player);
+
+    //the camera will follow the player in the world
+    this.game.camera.follow(this.player);
+
+    //move player with cursor keys
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+  },
+  //find objects in a Tiled layer that containt a property called "type" equal to a certain value
+  findObjectsByType: function(type, map, layer) {
+    var result = new Array();
+    map.objects[layer].forEach(function(element){
+      if(element.properties.type === type) {
+        //Phaser uses top left, Tiled bottom left so we have to adjust the y position
+        //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
+        //so they might not be placed in the exact pixel position as in Tiled
+        element.y -= map.tileHeight;
+        result.push(element);
+      }      
+    });
+    return result;
+  },
+  //create a sprite from an object
+  createFromTiledObject: function(element, group) {
+    var sprite = group.create(element.x, element.y, element.properties.sprite);
+      //copy all properties to the sprite
+      Object.keys(element.properties).forEach(function(key){
+        sprite[key] = element.properties[key];
+      });
+  },
+  update: function() {
+    this.game.physics.arcade.collide(this.player, this.blockedLayer);
+    var slot = 10;
+    if (this.state.moving && this.state.step < slot) {
+      this.state.step++;
+      return;
+    } else if (this.state.moving && this.state.step >= slot) {
+      this.state.moving = false;
+    }
+    this.player.body.velocity.y = 0;
+    this.player.body.velocity.x = 0;
+    var velocity = 100;
+    if (this.cursors.up.isDown) {
+      this.state.moving = true;
+      this.state.step = 0;
+      this.player.body.velocity.y -= velocity;
+    } else if (this.cursors.down.isDown) {
+      this.state.moving = true;
+      this.state.step = 0;
+      this.player.body.velocity.y += velocity;
+    } else if (this.cursors.left.isDown) {
+      this.state.moving = true;
+      this.state.step = 0;
+      this.player.body.velocity.x -= velocity;
+    } else if (this.cursors.right.isDown) {
+      this.state.moving = true;
+      this.state.step = 0;
+      this.player.body.velocity.x += velocity;
+    }
   }
 }
--- js/preload.js
+++ js/preload.js
@@ -5,6 +5,7 @@ gameObj.Preload.prototype = {
   preload: function() {
     this.load.tilemap('level1', 'assets/tilemaps/tilemaptest.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('gameTiles', 'assets/raw/test-tile2.png');
+    this.load.image('player', 'assets/raw/steve1.png');
   },
   create: function() {
     this.state.start('Game');
